bb301715a6c163163a7c1055f20fc7a3b9d9d56e
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-11-24 21:12:27 +0100

    feat: Enable dropdowns, extract nav partial, and add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  root 'chatroom#index'
+  root "chatroom#index"
 
-  get 'login', to: 'sessions#new'
+  get "login", to: "sessions#new"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
