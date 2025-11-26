c0e16fdbb75e8cbcca742e888c5f6e1f8332c7c2
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-11-26 10:22:02 +0100

    feat: Enable flash messages display

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-$(document).on('turbo:load', function() {
+$(document).on('turbo:load', function () {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').transition('fade');
+    });
 });
