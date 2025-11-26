df31a2f6de4a4ddd0e4c1a4a01461ab17e9084bc
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-11-26 13:20:23 +0100

    feat: Generate a Chatroom channel

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
   post 'message', to: 'messages#create'
+
+
+  mount ActionCable.server, at: "/cable"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
